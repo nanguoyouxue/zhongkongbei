c7f441610c5ece2ac20fe29c813357a3c6807d13
nanguoyouxue <47874078+nanguoyouxue@users.noreply.github.com>
2019-03-01 21:47:27 +0800

    Update 路径规划模拟器.cpp

--- 路径规划模拟器.cpp
+++ 路径规划模拟器.cpp
@@ -1,6 +1,9 @@
-//路径规划模拟器 1.0 beta
+//路径规划模拟器 1.1 beta
+/*
+实现功能：在地图中快速规划路线
+*/
 //by czl
-//2019/02/28/22:20
+//2019/02/29/21:46
 
 #include "stdafx.h"
 #include <stdio.h>
@@ -11,62 +14,513 @@
 #include <winnt.h>
 #include <iostream>
 /*全局函数与变量声明✧(≖ ◡ ≖✿ */
-void print();//用来输出经过的点
-int spot[12][12] = { {1,0,0,0,1,1,1,1,1,1,1,1},{1,666,0,0,0,666,666,666,666,666,666,1},{1,666,2333,0,0,0,2333,0,0,2333,0,1},{1,666,0,0,0,0,0,0,0,0,0,1},{1,666,0,0,1,1,1,1,0,0,0,1},{1,666,2333,0,1,1,1,1,0,0,666,1},{1,666,0,0,1,1,1,1,0,2333,666,1},{1,0,0,0,1,1,1,1,0,0,666,1},{1,0,0,0,0,0,0,0,0,0,666,1},{1,0,0,0,0,0,0,0,0,2333,666,1},{1,0,0,0,0,0,0,0,0,0,666,1},{1,1,1,1,1,1,1,1,0,0,0,1} };//0为可以到达的点，1为不可到达的点（包括货架与障碍物），待检查的障碍物可能点记为2333，待检查ABCD货架点为666,上层有空位则为6，下层有空则为8
+int spot[12][12] = { {1,1,1,1,1,1,1,1,1,1,1,1},{1,0,0,0,0,0,0,0,0,0,0,1},{1,0,0,0,0,0,0,0,0,0,0,1},{1,0,0,0,0,0,0,0,0,0,0,1},{1,0,0,0,1,1,1,1,0,0,0,1},{1,0,0,0,1,1,1,1,0,0,0,1},{1,0,0,0,1,1,1,1,0,0,0,1},{1,0,0,0,1,1,1,1,0,0,0,1},{1,0,0,0,0,0,0,0,0,0,0,1},{1,0,0,0,0,0,0,0,0,0,0,1},{1,0,0,0,0,0,0,0,0,0,0,1},{1,1,1,1,1,1,1,1,1,1,1,1} };//0为可以到达的点，1为不可到达的点（包括货架与障碍物）
 int flag0;//标记障碍物位置
-int scanner[20];//一个用来放乱七八糟东西的数组
-int m = 1, n = 8;//开始点
-int thing[9];//保存E货架上的物体
-int carbag[2] = { 0,0 };//暂定车上只能放两个物体
+int m, n, x, y,a,b;//分别是开始点和结束点和过程坐标
+int row;//排
+int line;//列
+//对几种情况的不同方案
+void Type1(int,int);
+void Type2(int,int);
+void Type3(int,int);
+void Type4(int,int);
+void Type5(int,int);
+void Type6(int,int);
+void Type7(int,int);
+void Type8(int,int);
+void Type14(int, int);
+void print(int,int);//打印坐标
+
 //-------(￣▽￣)／程序开始---------
 int main() {
-	scanf_s("%d %d %d\n", &scanner[0], &scanner[1], &scanner[2]);
-	scanf_s("%d %d %d\n", &scanner[3], &scanner[4], &scanner[5]);
-	scanf_s("%d %d %d\n", &scanner[6], &scanner[7], &scanner[8]);
-	spot[scanner[0]][scanner[1]] = scanner[2];
-	spot[scanner[3]][scanner[4]] = scanner[5];
-	spot[scanner[6]][scanner[7]] = scanner[8];
-	m += 1;//向左进一步
-	print();
-	printf("开始检测左方两个障碍物（若有障碍物输入它的编号，若无障碍物，则输入0）：\n");
-	scanf_s("%d", &flag0);//如果有障碍物输入它的编号，若无障碍物，则输入0
-	m += 1;
-	print();
-	m += 1;
-	print();
-	m += 1;
-	print();
-	printf("检测1号台上的物体（输入三个物体的编号）：\n");
-	scanf_s("%d %d %d", &thing[0], &thing[1], &thing[2]);
-	if (thing[0] <= 3) {
-		printf("抓取左边的物体\n");
-		carbag[0] = thing[0];
-	}
-	if (thing[1] <= 3) {
-		printf("抓取中间的物体\n");
-		if (carbag[0] == 0)carbag[0] = thing[1];
-		else carbag[1] = thing[1];
-	}
-	if (thing[2] <= 3 && carbag[1] == 0) {
-		printf("抓取右边的物体\n");
-		if (carbag[0] == 0)carbag[0] = thing[2];
-		else carbag[1] = thing[2];
-	}
-	m += 1;
-	print();
-	m += 1;
-	print();
-	printf("右转\n");
-	n += 1;
-	print();
-	printf("开始检测左方一个障碍物（若有障碍物输入它的编号，若无障碍物，则输入0）：\n");
-	scanf_s("%d", &flag0);//如果有障碍物输入它的编号，若无障碍物，则输入0
-	printf("左转\n");
-	m += 1;
-	print();
-	printf("右转\n");
+	scanf_s("%d %d", &m, &n);//输入开始点
+	scanf_s("%d %d", &x, &y);//输入结束点
+	scanf_s("%d", &flag0);//输入障碍点
 
+	switch (flag0) {//分情况给障碍物点赋值
+	case 1:spot[9][9] = 1; break;
+	case 2:spot[9][6] = 1; break;
+	case 3:spot[9][2] = 1; break;
+	case 4:spot[6][2] = 1; break;
+	case 5:spot[2][2] = 1; break;
+	case 6:spot[2][5] = 1; break;
+	}
+	row = m - x;
+	line = n - y;//计算横纵坐标差
+	a = m;
+	b = n;//开始在出发点
+
+	/*讨论十六种不同可能*/
+	if (row >= 0 && line >= 0 && abs(row) >= abs(line)&&(n+y)<=11)
+		Type1(a,b);
+	else if (row >= 0 && line >= 0 && abs(row) >= abs(line)&&(n+y)>11)
+		Type2(a,b);
+	else if (row < 0 && line < 0 && abs(row) >= abs(line)&& (n + y) <= 11)
+		Type3(a,b);
+	else if (row < 0 && line < 0 && abs(row) >= abs(line)&&(n+y)>11)
+		Type4(a,b);
+	else if (row >= 0 && line >= 0 && abs(row) < abs(line)&&(m+x)<=11)
+		Type5(a,b);
+	else if (row >= 0 && line >= 0 && abs(row) < abs(line)&&(m+x)>11)
+		Type6(a,b);
+	else if (row < 0 && line < 0 && abs(row) < abs(line)&&(m+x)<=11)
+		Type7(a,b);
+	else if (row < 0 && line < 0 && abs(row) < abs(line)&&(m+x)>11)
+		Type8(a,b);
+	else if (row >= 0 && line < 0 && abs(row) >= abs(line) && (n+y)<=11)
+		Type1(a, b);
+	else if (row >= 0 && line < 0 && abs(row) >= abs(line) && (n+y)>11)
+		Type2(a, b);
+	else if (row < 0 && line >= 0 && abs(row) >= abs(line) && (n+y)<=11)
+		Type3(a, b);
+	else if (row < 0 && line >= 0 && abs(row) >= abs(line) && (n+y)>11)
+		Type4(a, b);
+	else if (row >= 0 && line < 0 && abs(row) < abs(line) && (m + x)<=11)
+		Type5(a, b);
+	else if (row >= 0 && line < 0 && abs(row) < abs(line) && (m + x)>11)
+		Type14(a, b);
+	else if (row < 0 && line >= 0 && abs(row) < abs(line) && (m + x)<=11)
+		Type7(a, b);
+	else if (row < 0 && line >= 0 && abs(row) < abs(line) && (m + x)>11)
+		Type8(a, b);
+	
+	return 0;
 }
-void print() {
-	printf("[%d][%d]\n", m, n);//输出此刻所在的点
+
+void Type1(int a,int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (a > x) {
+		if (spot[a - 1][b] == 1) {
+			b -= 1;
+		}
+		else {
+			a -= 1;
+		}
+		print(a,b);
+		Type1(a, b);
+		return;
+	}
+	else if (b < y) {
+		if (spot[a][b + 1] == 1) {
+			a -= 1;
+		}
+		else {
+			b += 1;
+		}
+		print(a,b);
+		Type1(a, b);
+		return;
+	}
+	else if (b > y) {
+		if (spot[a][b - 1] == 1) {
+			a -= 1;
+		}
+		else {
+			b -= 1;
+		}
+		print(a, b);
+		Type1(a, b);
+		return;
+	}
+	else if (b==y) {
+		a += 1;
+		print(a,b);
+		Type1(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type2(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (a > x) {
+		if (spot[a - 1][b] == 1) {
+			b += 1;
+		}
+		else {
+			a -= 1;
+		}
+		print(a, b);
+		Type2(a, b);
+		return;
+	}
+	else if (b > y) {
+		if (spot[a][b - 1] == 1) {
+			a -= 1;
+		}
+		else {
+			b -= 1;
+		}
+		print(a, b);
+		Type2(a, b);
+		return;
+	}
+	else if (b < y) {
+		if (spot[a][b + 1] == 1) {
+			a -= 1;
+		}
+		else {
+			b += 1;
+		}
+		print(a, b);
+		Type2(a, b);
+		return;
+	}
+	else if (b == y) {
+		a += 1;
+		print(a, b);
+		Type2(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type3(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (a < x) {
+		if (spot[a + 1][b] == 1) {
+			b -= 1;
+		}
+		else {
+			a += 1;
+		}
+		print(a, b);
+		Type3(a, b);
+		return;
+	}
+	else if (b < y) {
+		if (spot[a][b + 1] == 1) {
+			a += 1;
+		}
+		else {
+			b += 1;
+		}
+		print(a, b);
+		Type3(a, b);
+		return;
+	}
+	else if (b > y) {
+		if (spot[a][b - 1] == 1) {
+			a += 1;
+		}
+		else {
+			b -= 1;
+		}
+		print(a, b);
+		Type3(a, b);
+		return;
+	}
+	else if (b == y) {
+		a -= 1;
+		print(a, b);
+		Type3(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type4(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (a < x) {
+		if (spot[a + 1][b] == 1) {
+			b += 1;
+		}
+		else {
+			a += 1;
+		}
+		print(a, b);
+		Type4(a, b);
+		return;
+	}
+	else if (b < y) {
+		if (spot[a][b + 1] == 1) {
+			a += 1;
+		}
+		else {
+			b += 1;
+		}
+		print(a, b);
+		Type4(a, b);
+		return;
+	}
+	else if (b > y) {
+		if (spot[a][b - 1] == 1) {
+			a += 1;
+		}
+		else {
+			b -= 1;
+		}
+		print(a, b);
+		Type4(a, b);
+		return;
+	}
+	else if (b == y) {
+		a -= 1;
+		print(a, b);
+		Type4(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type5(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (b >y) {
+		if (spot[a][b-1] == 1) {
+			a-= 1;
+		}
+		else {
+			b-= 1;
+		}
+		print(a, b);
+		Type5(a, b);
+		return;
+	}
+	else if (a < x) {
+		if (spot[a+1][b] == 1) {
+			b-= 1;
+		}
+		else {
+			a += 1;
+		}
+		print(a, b);
+		Type5(a, b);
+		return;
+	}
+	else if (a > x) {
+		if (spot[a-1][b] == 1) {
+			b-= 1;
+		}
+		else {
+			a -= 1;
+		}
+		print(a, b);
+		Type5(a, b);
+		return;
+	}
+	else if (a == x) {
+		b+= 1;
+		print(a, b);
+		Type5(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type6(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (b >y) {
+		if (spot[a][b - 1] == 1) {
+			a -= 1;
+		}
+		else {
+			b -= 1;
+		}
+		print(a, b);
+		Type6(a, b);
+		return;
+	}
+	else if (a < x) {
+		if (spot[a + 1][b] == 1) {
+			b -= 1;
+		}
+		else {
+			a += 1;
+		}
+		print(a, b);
+		Type6(a, b);
+		return;
+	}
+	else if (a > x) {
+		if (spot[a - 1][b] == 1) {
+			b -= 1;
+		}
+		else {
+			a -= 1;
+		}
+		print(a, b);
+		Type6(a, b);
+		return;
+	}
+	else if (a == x) {
+		b += 1;
+		print(a, b);
+		Type6(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type7(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (b <y) {
+		if (spot[a][b + 1] == 1) {
+			//printf("spot[%d][%d]=%d",a,b+1, spot[a][b + 1]);
+			a -= 1;
+		}
+		else {
+			b += 1;
+		}
+		print(a, b);
+		Type7(a, b);
+		return;
+	}
+	else if (a < x) {
+		if (spot[a + 1][b] == 1) {
+			b += 1;
+		}
+		else {
+			a += 1;
+		}
+		print(a, b);
+		Type7(a, b);
+		return;
+	}
+	else if (a > x) {
+		if (spot[a - 1][b] == 1) {
+			b += 1;
+		}
+		else {
+			a -= 1;
+		}
+		print(a, b);
+		Type7(a, b);
+		return;
+	}
+	else if (a == x) {
+		b -= 1;
+		print(a, b);
+		Type7(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type8(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (b <y) {
+		if (spot[a][b + 1] == 1) {
+			a += 1;
+		}
+		else {
+			b += 1;
+		}
+		print(a, b);
+		Type8(a, b);
+		return;
+	}
+	else if (a < x) {
+		if (spot[a + 1][b] == 1) {
+			b += 1;
+		}
+		else {
+			a += 1;
+		}
+		print(a, b);
+		Type8(a, b);
+		return;
+	}
+	else if (a > x) {
+		if (spot[a - 1][b] == 1) {
+			b += 1;
+		}
+		else {
+			a -= 1;
+		}
+		print(a, b);
+		Type8(a, b);
+		return;
+	}
+	else if (a == x) {
+		b -= 1;
+		print(a, b);
+		Type8(a, b);
+		return;
+	}
+
+	return;
+}
+
+void Type14(int a, int b) {
+	if (a == x && b == y) {//到达结束点后退出递归
+		printf("到达！\n");
+		return;
+	}
+	if (b <y) {
+		if (spot[a][b + 1] == 1) {
+			a += 1;
+		}
+		else {
+			b += 1;
+		}
+		print(a, b);
+		Type14(a, b);
+		return;
+	}
+	else if (a < x) {
+		if (spot[a + 1][b] == 1) {
+			b -= 1;
+		}
+		else {
+			a += 1;
+		}
+		print(a, b);
+		Type14(a, b);
+		return;
+	}
+	else if (a > x) {
+		if (spot[a - 1][b] == 1) {
+			b -= 1;
+		}
+		else {
+			a -= 1;
+		}
+		print(a, b);
+		Type14(a, b);
+		return;
+	}
+	else if (a == x) {
+		b -= 1;
+		print(a, b);
+		Type14(a, b);
+		return;
+	}
+
+	return;
+}
+
+
+void print(int a,int b) {
+	printf("[%d,%d]\n", a, b);
 }
