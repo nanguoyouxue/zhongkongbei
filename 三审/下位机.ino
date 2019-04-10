8daf45ca57aec67fca0bb9b0fdc2897f38f7db62
南国有雪 <47874078+nanguoyouxue@users.noreply.github.com>
2019-04-10 18:44:50 +0800

    Update 下位机.ino

--- 三审/下位机.ino
+++ 三审/下位机.ino
@@ -1,60 +1,55 @@
 /*
-* 下位机 1.1 beta
+* 下位机 1.2 beta
+* 这个版本完整封装和整理了各个函数，并添加注释，但还未经过测试
 * by czl & robin
-*这是一个测试版本，用于后期修改出错时补救
-* 2019/04/10
+* 2019/04/03
 */
-int val1, val2;
-int CompassAngle0, CompassAngle1, CompassAngle2;
-int delta(int val1, int val2);
-int zhinan();
-#include <SoftwareSerial.h>//软串口通信库函数
 
+//引用的库函数
+#include <SoftwareSerial.h>//软串口通信库函数（用于指南针）
 SoftwareSerial mySerial(8, 9); //指南针模块的RX接11脚 TX接10脚
 
-                 //全局函数声明
+//全局函数声明--控制车运动的函数括号里为持续的时间，单位毫秒--
 void forward(int);//前进
 void backward(int);//后退
-void left(int);//向左
-void right(int);//向右
-void stoping(int);//刹车
+void left(int);//向左，函数内已清除了使能端
+void right(int);//向右，函数内已清除了使能端
+void stoping(int);//刹车，函数内已清除了使能端
 void clearing();//关闭使能端口
-        //--控制车运动的函数括号里为持续的时间，单位毫秒--
+int delta(int, int);//检测转弯时角度是否达标
+int zhinan();//用指南针测量目前方向角度
 
-        //arduino引脚声明
+//arduino引脚声明
 byte rightwheel0 = 5;
 byte rightwheel1 = 7;
 byte leftwheel0 = 4;
 byte leftwheel1 = 2;
-byte righten = 6;//使能端（右轮）
-byte leften = 3;//使能端（左轮）
+byte righten = 6;//使能端
+byte leften = 3;//使能端
 
-        //全局变量声明
+//全局变量声明
+int val1, val2;//记录转弯前后角度值
+int CompassAngle0, CompassAngle1, CompassAngle2;
 
-        //主函数开始
+
+//主函数开始
 void setup() {
-  for (byte i = 2; i <= 7; i++)
-    pinMode(i, OUTPUT);//定义接口
-  Serial.begin(9600);//与电脑硬串口波特率
-  mySerial.begin(9600);//软串口波特率
+  //接口定义
+  for (byte i = 2; i <= 7; i++)pinMode(i, OUTPUT);//定义2-7接口为输出
+
+  //串口定义
+  Serial.begin(9600);//与电脑硬串口波特率9600
+  mySerial.begin(9600);//指南针软串口波特率9600
+
+
+  //初始化
+  clearing();//关闭使能端口作为初始化
+  delay(3000);//随便写的停顿
 }
 
 void loop() {
-  clearing();//关闭使能端口作为初始化
-  delay(3000);
-  Serial.print("第一次测量得到：");
-  int val1 = zhinan();
-  right(1000);
-  clearing();
-  Serial.print("第二次测量得到：");
-  int val2 = zhinan();
-  while (delta(val1, val2) <= 85) {
-    right(100);
-    clearing();
-    val2 = zhinan();
-  }
-  clearing();
-  delay(5000);
+ right(1200);
+ delay(5000);
 }
 
 //封装函数定义
@@ -87,6 +82,10 @@ void backward(int microseconds) {
 }
 
 void left(int microseconds) {
+  val1 = zhinan();//第一次测量角度值
+  Serial.print("第一次测量得到：");
+  Serial.println(val1);//输出测量得到的角度
+  
   digitalWrite(leftwheel0, HIGH);
   digitalWrite(leftwheel1, LOW);
   analogWrite(leften, 255);
@@ -97,10 +96,31 @@ void left(int microseconds) {
   analogWrite(righten, 255);
   //右边轮子正转
 
-  delay(microseconds);
-}
+  delay(microseconds);//延时microseconds毫秒
+  clearing();//停车等待角度测量
 
+  val2 = zhinan();//第一次测量角度值
+  Serial.print("第二次测量得到：");
+  Serial.println(val2);
+
+  while (delta(val1, val2) <= 85) {//最大允许误差和修正值后期视实际情况修改,如果转弯角度不合理，则修正
+    left(100);
+    clearing();
+    val2 = zhinan();//再次测量现在的角度
+  }
+  while (delta(val1, val2) > 95) {//最大允许误差和修正值后期视实际情况修改
+    right(50);
+    clearing();
+    val2 = zhinan();//再次测量现在的角度
+  }
+
+  clearing();
+}
 void right(int microseconds) {
+  val1 = zhinan();//第一次测量角度值
+  Serial.print("第一次测量得到：");
+  Serial.println(val1);//输出测量得到的角度
+  
   digitalWrite(leftwheel0, LOW);
   digitalWrite(leftwheel1, HIGH);
   analogWrite(leften, 255);
@@ -111,19 +131,36 @@ void right(int microseconds) {
   analogWrite(righten, 255);
   //右边轮子反转
 
-  delay(microseconds);
+  delay(microseconds);//延时microseconds毫秒
+  clearing();//停车等待角度测量
+
+  val2 = zhinan();//第一次测量角度值
+  Serial.print("第二次测量得到：");
+  Serial.println(val2);
+
+  while (delta(val1, val2) <= 85) {//最大允许误差和修正值后期视实际情况修改,如果转弯角度不合理，则修正
+    right(100);
+    clearing();
+    val2 = zhinan();//再次测量现在的角度
+  }
+  while (delta(val1, val2) > 95) {//最大允许误差和修正值后期视实际情况修改
+    left(50);
+    clearing();
+    val2 = zhinan();//再次测量现在的角度
+  }
+
+  clearing();
 }
 
 void stoping(int microseconds) {
   digitalWrite(leftwheel0, HIGH);
   digitalWrite(leftwheel1, HIGH);
   analogWrite(leften, 255);
-  //左边轮子反转
 
   digitalWrite(rightwheel0, HIGH);
   digitalWrite(rightwheel1, HIGH);
   analogWrite(righten, 255);
-  //右边轮子正转
+  
   delay(150);//刹车用时150ms
 
   clearing();//关闭使能端
@@ -143,12 +180,10 @@ int delta(int val1, int val2) {
     return abs(val1 - val2);
 }
 
-int zhinan()
-{
-loop:mySerial.write(byte(0x31));//发送指令
+int zhinan(){
+  loop:mySerial.write(byte(0x31));//发送指令
   delay(200);
-  if (mySerial.available())//共返回8个字节
-  {
+  if (mySerial.available()){//共返回8个字节
     byte Byte0 = mySerial.read();//开始读取缓冲区里的字节
     byte Byte1 = mySerial.read();//byte型变量为字节型
     byte Byte2 = mySerial.read();
@@ -158,17 +193,12 @@ loop:mySerial.write(byte(0x31));//发送指令
     byte Byte6 = mySerial.read();
     byte Byte7 = mySerial.read();
     CompassAngle1 = (Byte2 - 0x30) * 100 + (Byte3 - 0x30) * 10 + (Byte4 - 0x30);//算出角度
-    //Serial.println(CompassAngle1);
-    if (CompassAngle1<0 || CompassAngle1>360)goto loop;
   }
+  if (CompassAngle1<0 || CompassAngle1>360)goto loop;//如果收到异常值，重新测量
 
-    //while (Serial.available())
-      //byte Byte0 = Serial.read();//清空缓冲区
-
-    mySerial.write(byte(0x31));//发送指令
+  mySerial.write(byte(0x31));//发送指令，第二次测量角度
   delay(200);
-  if (mySerial.available())//共返回8个字节
-  {
+  if (mySerial.available()){//共返回8个字节
     byte Byte0 = mySerial.read();//开始读取缓冲区里的字节
     byte Byte1 = mySerial.read();//byte型变量为字节型
     byte Byte2 = mySerial.read();
@@ -178,16 +208,11 @@ loop:mySerial.write(byte(0x31));//发送指令
     byte Byte6 = mySerial.read();
     byte Byte7 = mySerial.read();
     CompassAngle2 = (Byte2 - 0x30) * 100 + (Byte3 - 0x30) * 10 + (Byte4 - 0x30);//算出角度
-    //Serial.println(CompassAngle2);
-    if (CompassAngle2 <= 0 || CompassAngle2 >= 360)goto loop;
   }
-
-    if (abs(CompassAngle2 - CompassAngle1) >= 5)goto loop;
-    else CompassAngle0 = 0.5*(CompassAngle1 + CompassAngle2);
-    Serial.println(CompassAngle0);
-
-
-    return CompassAngle0;
-
-
-  }
+  if (CompassAngle2 < 0 || CompassAngle2 > 360)goto loop;//如果收到异常值，重新测量
+    
+  if (abs(CompassAngle2 - CompassAngle1) >= 5)goto loop;//如果两次测出的值相差较大，重新测量
+    else CompassAngle0 = 0.5*(CompassAngle1 + CompassAngle2);//求出最终值
+    
+  return CompassAngle0;
+}
