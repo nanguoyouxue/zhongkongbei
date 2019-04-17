01b6dc4733fc14e97855db1bba4eb2a04f2732c3
南国有雪 <47874078+nanguoyouxue@users.noreply.github.com>
2019-04-17 11:00:14 +0800

    Update 串口通信可行性验证增强版（下位机）.ino

--- 串口通信可行性验证增强版（下位机）.ino
+++ 串口通信可行性验证增强版（下位机）.ino
@@ -1,24 +1,28 @@
-//实现arduino返回电脑发送的指令
-//by czl
-//2019/03/21/9:17
+/*arduino接收电脑指令
+ * by czl
+ * 2019/04/17
+ */
 
-String comdata = "";
-
-void setup()
-{
-    Serial.begin(9600);
+String inString = "";
+void  setup(){
+  Serial.begin(9600);
 }
-
-void loop()
-{
-    while (Serial.available() > 0)  
-    {
-        comdata += char(Serial.read());
-        delay(2);
-    }
-    if (comdata.length() > 0)
-    {
-        Serial.println(comdata);
-        comdata = "";
+void loop(){
+  uartReceive();
+  if(inString[0]=='1'&&inString[1]=='2'&&inString[2]=='3')Serial.println("hello world");
+  inString="";
+  delay(500);
+}
+//接收串口发来的字符串
+void uartReceive(){        
+    while (Serial.available()>0) {   //如果串口有数据
+        char inChar = Serial.read(); //读取串口字符
+        //inString += inChar;
+        inString.concat(inChar);     //连接接收到的字符组
+        delayMicroseconds(100);      //为了防止数据丢失,在此设置短暂延时100us
+        Serial.flush();             //清空串口接收缓存
+        if(inString.length() > 200){
+            inString ="";
+        }
     }
 }
