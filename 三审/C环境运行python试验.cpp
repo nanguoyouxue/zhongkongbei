b96fe76d723002c9795782a669274fa027b74a78
南国有雪 <47874078+nanguoyouxue@users.noreply.github.com>
2019-05-01 22:24:41 +0800

    Update C环境运行python试验.cpp

--- 三审/C环境运行python试验.cpp
+++ 三审/C环境运行python试验.cpp
@@ -1,4 +1,3 @@
-
 #include "stdafx.h"
 #include <stdio.h>
 #include <windows.h>
@@ -7,10 +6,31 @@
 #include <conio.h>
 #include <winnt.h>
 #include <iostream>
+#include <time.h>
+#include<stdbool.h>
+#include<pthread.h>        //包含多线程支持库头文件
+#pragma comment(lib, "pthreadVC2.lib")//这句话别忘了
 
 using namespace std;
 
-int main() {
-  system("python helloworld.py");
-  return 0;
+pthread_t t1;                           //pthread_t变量t1，用于存储线程ID
+
+void* My_thread(void* args) {            //线程运行函数
+	clock_t start = clock();
+	system("python E:\\lalala\\classify_image_inception_v3.py");
+	clock_t end = (clock() - start) / CLOCKS_PER_SEC;
+	cout << "time comsumption is " << end << endl;
+	return NULL;
+}
+
+int main() {                              //主程序
+	pthread_create(&t1, NULL, My_thread, NULL);  //创建缺省线程
+	printf_s("我是主线程\n");
+	Sleep(1000);
+	printf_s("我真的是主线程\n");
+	Sleep(1000);
+	printf_s("我还是主线程\n");
+	Sleep(10000);
+	printf("777");
+	return 0;
 }
