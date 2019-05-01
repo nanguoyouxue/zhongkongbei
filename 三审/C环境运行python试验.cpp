1cbf835a443718b893fd80bfc5e9d36791d56d02
南国有雪 <47874078+nanguoyouxue@users.noreply.github.com>
2019-05-01 21:01:01 +0800

    Create C环境运行python试验.cpp

---
+++ 三审/C环境运行python试验.cpp
@@ -0,0 +1,16 @@
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <winnt.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  system("python helloworld.py");
+  return 0;
+}
