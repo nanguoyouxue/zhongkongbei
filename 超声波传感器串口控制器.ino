feda57d10d8c3dfc21dab61db31c252742b8cbd0
南国有雪 <47874078+nanguoyouxue@users.noreply.github.com>
2019-05-01 12:38:52 +0800

    Update 超声波传感器串口控制器.ino

--- 超声波传感器串口控制器.ino
+++ 超声波传感器串口控制器.ino
@@ -1,23 +1,23 @@
-/*超声波传感器串口控制器 1.0
+/*超声波传感器串口控制器 1.1 beta
  * by czl
- * 2019/04/24
+ * 2019/05/01
  */
-const int pingPin = 11; //Trig接11号引脚
-const int echoPin = 12; // Echo接12号引脚
+byte pingPin = 11; //Trig接11号引脚
+byte echoPin = 12; // Echo接12号引脚
 
 void setup() {
    Serial.begin(9600);
+   pinMode(pingPin, OUTPUT);
+   pinMode(echoPin, INPUT);
 }
 
 void loop() {
-   long duration, inches, cm;
-   pinMode(pingPin, OUTPUT);
+   int duration, cm;   //事实上我们并用不到如此高的精度，所以直接用整型就够了
    digitalWrite(pingPin, LOW);
    delayMicroseconds(2);
    digitalWrite(pingPin, HIGH);
    delayMicroseconds(10);
    digitalWrite(pingPin, LOW);
-   pinMode(echoPin, INPUT);
    duration = pulseIn(echoPin, HIGH);
    cm =duration / 29 / 2 ;
    Serial.print(cm);//以厘米为单位
