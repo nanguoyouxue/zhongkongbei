2d52dace1a232b2e82e3e9fc578acc46e26560fe
南国有雪 <47874078+nanguoyouxue@users.noreply.github.com>
2019-05-06 17:27:56 +0800

    Update 超声波传感器控制器.ino

--- 超声波传感器控制器.ino
+++ 超声波传感器控制器.ino
@@ -1,14 +1,14 @@
-/*超声波传感器串口控制器 1.1 beta
+/*超声波传感器串口控制器 1.1
  * by czl
- * 2019/05/01
+ * 2019/05/05
  * 1.本质上不是串口通信所以删去名字中的“串口”
  * 2.为减少测量时间，缩短了测量之间的时间差
-       @是否会影响测量准确性
-       @是否会导致误差值连续出现
-       待验证
+       @是否会影响测量准确性-否
+       @是否会导致误差值连续出现-距离较远时可能会出现不稳定
+
  */
 byte pingPin = 11; //Trig接11号引脚
-byte echoPin = 12; // Echo接12号引脚
+byte echoPin = 10; // Echo接10号引脚
 
 void setup() {
    Serial.begin(9600);
@@ -25,6 +25,6 @@ void loop() {
    digitalWrite(pingPin, LOW);
    duration = pulseIn(echoPin, HIGH);
    cm =duration / 29 / 2 ;
-   Serial.print(cm);//以厘米为单位
+   Serial.println(cm);//以厘米为单位
    delay(100);
 }
